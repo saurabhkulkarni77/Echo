1e88d06d680095123335b99490f96aae7aa3d57e
saurabhkulkarni77 <sskkulk@gmail.com>
2018-12-27 01:55:56 -0500

    Update model.py

--- model.py
+++ model.py
@@ -4,10 +4,11 @@ from keras.preprocessing import sequence
 from keras.models import load_model
 from keras.datasets import imdb
 max_review_length = 1600
+text_to_predict = "i am loving it"
 
 word2index = imdb.get_word_index()
 test=[]
-for word in word_tokenize( "i am loving it"):
+for word in word_tokenize(text_to_predict):
      test.append(word2index[word])
 model = load_model("model.h5")
 test=sequence.pad_sequences([test],maxlen=max_review_length)
