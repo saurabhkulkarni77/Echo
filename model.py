f49013668827958e76a9d7e76449b892e0f9788e
saurabhkulkarni77 <sskkulk@gmail.com>
2018-12-27 02:06:05 -0500

    Update model.py

--- model.py
+++ model.py
@@ -1,10 +1,12 @@
-
+'''
+if prediceted value is more than 0.5 then survey taker is happy about the company 
+'''
 from nltk import word_tokenize
 from keras.preprocessing import sequence
 from keras.models import load_model
 from keras.datasets import imdb
 max_review_length = 1600
-text_to_predict = "i am loving it"
+text_to_predict = "i am loving my work"
 
 word2index = imdb.get_word_index()
 test=[]
